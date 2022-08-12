50c2c3d535a5f253e9cd3e20cd59a270c775d54b
khushi <khushi7mittal@gmail.com>
2022-08-12 18:23:38 +0530

    first commit

--- backend/models/exercise.models.js
+++ backend/models/exercise.models.js
@@ -1,16 +1,18 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 
 const Schema = mongoose.Schema;
 
-const exerciseSchema = new Schema({
-    username: {type: String, required: true },
-    description: { type:String, required: true },
+const exerciseSchema = new Schema(
+  {
+    username: { type: String, required: true },
+    description: { type: String, required: true },
     duration: { type: Number, required: true },
-    date: {type: Date, required:true } ,
-}, {
-    timestamps: true
-}
+    date: { type: Date, required: true },
+  },
+  {
+    timestamps: true,
+  }
 );
 
-const Exercise = mongoose.model('Exercise', exerciseSchema);
+const Exercise = mongoose.model("Exercise", exerciseSchema);
 module.exports = Exercise;
